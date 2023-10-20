49615ffa214c5864b3962efbfd78d857b3803c8a
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-10-20 12:10:22 -0700

    finishes bare ui, zero btn unfinished

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,36 +6,42 @@ function App() {
   return (
     <div>
       <h1 className='title' >Calculator</h1>
-      
+        <div className='ans-container'>
+          <div className='ans'>{answer}</div>
+        </div>
 
-      <div className='calc-container'>
-        <button className='input'>AC</button>
-        <button className='input'>+/-</button>
-        <button className='input'>%</button>
-        <button className='input'>÷</button>
+        <div className='input-container'>
+          <button name='clear' onClick={handleClick} className='input'>AC</button>
+          <button name='negative' onClick={handleClick} className='input'>+/-</button>
+          <button name='percent' onClick={handleClick} className='input'>%</button>
+          <button name='divide' onClick={handleClick} className='input'>÷</button>
 
-        <button className='input'>7</button>
-        <button className='input'>8</button>
-        <button className='input'>9</button>
-        <button className='input'>x</button>
-        
-        <button className='input'>4</button>
-        <button className='input'>5</button>
-        <button className='input'>6</button>
-        <button className='input'>-</button>
+          <button name='seven' onClick={handleClick} className='input'>7</button>
+          <button name='eight' onClick={handleClick} className='input'>8</button>
+          <button name='nine' onClick={handleClick} className='input'>9</button>
+          <button name='multiply' onClick={handleClick} className='input'>x</button>
+          
+          <button name='four' onClick={handleClick} className='input'>4</button>
+          <button name='five' onClick={handleClick} className='input'>5</button>
+          <button name='six' onClick={handleClick} className='input'>6</button>
+          <button name='subtract' onClick={handleClick} className='input'>-</button>
 
-        <button className='input'>1</button>
-        <button className='input'>2</button>
-        <button className='input'>3</button>
-        <button className='input'>+</button>
+          <button name='one' onClick={handleClick} className='input'>1</button>
+          <button name='two' onClick={handleClick} className='input'>2</button>
+          <button name='three' onClick={handleClick} className='input'>3</button>
+          <button name='add' onClick={handleClick} className='input'>+</button>
 
-        <button className='special-input'>0</button>
-        <div className='empty-cell'></div>
-        <button className='input'>.</button>
-        <button className='input'>=</button>
-      </div>
+          <button name='zero' onClick={handleClick} className='special-input'>0</button>
+          <div name='zero' onClick={handleClick} className='empty-cell'></div>
+          <button name='decimal' onClick={handleClick} className='input'>.</button>
+          <button name='equals' onClick={handleClick} className='input btm-right'>=</button>
+        </div>
     </div>
   )
+
+  function handleClick(e){
+    console.log(e.target.name)
+  }
 }
 
 export default App;
