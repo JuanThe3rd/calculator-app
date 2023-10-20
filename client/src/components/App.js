dbbff94c45b58be093fac98c848e621b266a689e
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-10-20 11:46:49 -0700

    creates input grid

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,41 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import React, {useState} from "react";
 
 function App() {
-  return <h1>Phase 4 Project Client</h1>;
+  const [answer, setAnswer] = useState(0);
+
+  return (
+    <div>
+      <h1 className='title' >Calculator</h1>
+      
+
+      <div className='calc-container'>
+        <button className='input'>AC</button>
+        <button className='input'>+/-</button>
+        <button className='input'>%</button>
+        <button className='input'>÷</button>
+
+        <button className='input'>7</button>
+        <button className='input'>8</button>
+        <button className='input'>9</button>
+        <button className='input'>x</button>
+        
+        <button className='input'>4</button>
+        <button className='input'>5</button>
+        <button className='input'>6</button>
+        <button className='input'>-</button>
+
+        <button className='input'>1</button>
+        <button className='input'>2</button>
+        <button className='input'>3</button>
+        <button className='input'>+</button>
+
+        <button className='special-input'>0</button>
+        <div className='empty-cell'></div>
+        <button className='input'>.</button>
+        <button className='input'>=</button>
+      </div>
+    </div>
+  )
 }
 
 export default App;
---
+++ server/migrations/versions/ed5c9a2c38c3_create_db.py
@@ -0,0 +1,24 @@
+"""Create db
+
+Revision ID: ed5c9a2c38c3
+Revises: 
+Create Date: 2023-10-19 16:36:19.538186
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ed5c9a2c38c3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
